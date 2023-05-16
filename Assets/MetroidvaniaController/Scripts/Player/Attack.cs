5bef09de92a8efbb4dc3c2b8fe0f679f4ac2232e
TienLee2 <nguyenletien21052002@gmail.com>
2023-05-16 13:44:12 +0700

    First Project Push

---
+++ Assets/MetroidvaniaController/Scripts/Player/Attack.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+	//damage
+	public float dmgValue = 4;
+	//Mũi tên để bắn
+	public GameObject throwableObject;
+
+	public Transform attackCheck;
+
+	//Cái để điều khiển animation
+	public Animator animator;
+	//bool 
+	public bool canAttack = true;
+
+	public bool isTimeToCheck = false;
+	//Camera
+	public GameObject cam;
+
+
+    private void Update()
+    {
+		//Nhấn X và tấn công
+		if (Input.GetKeyDown(KeyCode.X) && canAttack)
+		{
+			//chỉnh bool false để ko đánh nhiều lần được
+			canAttack = false;
+			//set animator tấn công
+			animator.SetBool("IsAttacking", true);
+			//đếm ngược thời gian để tiếp tục tấn công
+			StartCoroutine(AttackCooldown());
+		}
+
+		//Nhấn V để bắn, có thể tạo thêm code để đếm ngược thời gian có thể bắn
+		if (Input.GetKeyDown(KeyCode.V))
+		{
+			//Tạo object cung tên
+			GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f,-0.2f), Quaternion.identity) as GameObject; 
+			//điều khiển hướng di chuyển của tên
+			Vector2 direction = new Vector2(transform.localScale.x, 0);
+			throwableWeapon.GetComponent<ThrowableWeapon>().direction = direction; 
+			throwableWeapon.name = "ThrowableWeapon";
+		}
+	}
+
+	IEnumerator AttackCooldown()
+	{
+		yield return new WaitForSeconds(0.25f);
+		canAttack = true;
+	}
+
+	public void DoDashDamage()
+	{
+		dmgValue = Mathf.Abs(dmgValue);
+		Collider2D[] collidersEnemies = Physics2D.OverlapCircleAll(attackCheck.position, 0.9f);
+		for (int i = 0; i < collidersEnemies.Length; i++)
+		{
+			if (collidersEnemies[i].gameObject.tag == "Enemy")
+			{
+				if (collidersEnemies[i].transform.position.x - transform.position.x < 0)
+				{
+					dmgValue = -dmgValue;
+				}
+				collidersEnemies[i].gameObject.SendMessage("ApplyDamage", dmgValue);
+				cam.GetComponent<CameraFollow>().ShakeCamera();
+			}
+		}
+	}
+}
---
+++ Assets/MetroidvaniaController/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	//tốc độ đuổi theo nhân vật của camera
+	public float FollowSpeed = 2f;
+	//vị trí nhân vật
+	public Transform Target;
+
+	//vị trí của camera để lắc mỗi khi đánh
+	private Transform camTransform;
+
+	//thời gian shake camera
+	public float shakeDuration = 0f;
+
+	// Sức mạnh khi lắc camera, giá trị càng lớn lắc càng mạnh
+	public float shakeAmount = 0.1f;
+	public float decreaseFactor = 1.0f;
+
+	Vector3 originalPos;
+
+	void Awake()
+	{
+		//set con trỏ chuột biến mất
+		Cursor.visible = false;
+		//Nếu vị trí của cam ko có
+		if (camTransform == null)
+		{
+			//lấy vị trí của vật đang giữ script gán vào vị trí lắc
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+
+	void OnEnable()
+	{
+		//khi bật script, gán vị trí local của cam vào một cái vector3
+		originalPos = camTransform.localPosition;
+	}
+
+	private void Update()
+	{
+		//tạo một cái vector3 có giá trị là vị trí hiện tại của nhân vật
+		Vector3 newPosition = Target.position;
+		// trục z của vector3 giảm đi 10
+		newPosition.z = -10;
+		// Slerp(a,b,t) di chuyển từ a đến b với thời gian t
+		transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+
+		//Nếu có thời gian shake thì lắc camera
+		if (shakeDuration > 0)
+		{
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		}
+	}
+
+	public void ShakeCamera()
+	{
+		originalPos = camTransform.localPosition;
+		shakeDuration = 0.2f;
+	}
+}
